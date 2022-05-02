8693defeb30787da1b431b611c25ee8f61fe663a
Maria Clara <mcfranca0107@gmail.com>
2022-05-02 15:10:23 -0300

    testes

--- lib/model.dart
+++ lib/model.dart
@@ -4,28 +4,42 @@ class Pessoa {
   String? nome;
   String? cpf;
   DateTime? nascimento;
-  int? idade;
   Sexo? sexo;
-  double? salario;
-  bool? programador;
+
+  int? getIdade() {
+    if (nascimento == null) {
+      return null;
+    }
+    final hoje = DateTime.now();
+    final idade = hoje.difference(nascimento!);
+    return idade.inDays ~/ 365;
+  }
 
   @override
   String toString() {
-    return "nome: $nome, cpf: $cpf, nascimento: $nascimento, idade: $idade, sexo: $sexo, "
-        "salario: $salario, é programador? $programador";
+    return "nome: $nome, cpf: $cpf, nascimento: $nascimento, idade: ${getIdade()}, sexo: $sexo";
+  }
+}
+
+class Programador extends Pessoa {
+  double? salario;
+
+  @override
+  String toString() {
+    return "nome: $nome, cpf: $cpf, nascimento: $nascimento, idade: ${getIdade()}, sexo: $sexo,"
+        " salario: $salario";
   }
 }
 
 void mainEntidades() {
-  Pessoa pessoa = Pessoa();
-
+  Programador pessoa = Programador();
   pessoa.nome = "Letícia";
   pessoa.cpf = "090.878.389-21";
   pessoa.nascimento = DateTime(2005, 3, 22);
-  pessoa.idade = 17;
   pessoa.sexo = Sexo.FEMININO;
-  pessoa.salario = 10500.00;
-  pessoa.programador = true;
+  //pessoa.salario = 1500.00;
 
   print(pessoa);
 }
+
+
